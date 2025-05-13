07772208f110e3394b3b679b43867df390780800
b4nzaii <Mans200@live.se>
2025-05-13 16:30:38 +0200

    startat projektet, initierat bootstrap

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+
+
+const HomePage = () => {
+    return (
+        <Container className="mt-4">
+            <h2>FYP</h2> // Här ska For you Page vara
+        </Container>
+    )
+}
+
+export default HomePage;
